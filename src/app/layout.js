88014b96010415209ca19457c073315a5f180c3a
allistair <allistair.jak@gmail.com>
2024-06-10 17:52:59 +0200

    ci: create the base file for html templating

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,25 @@
 import styles from './build/App.css'
 
 export const metadata = {
-  title: "Adhyanga",
-  description: "Bienvenue sur adhyanga votre cabinet de soin en ligne",
+  title: "Adhyanga - Votre cabinet de soin kinésiologique et ayurvédique sur Angoulême",
+  description: "Adhyanga - Cabinet de soin kinésiologique et ayurvédique à Angoulême. Découvrez une approche holistique avec Adeline Pierrot, praticienne expérimentée, pour harmoniser votre bien-être physique et émotionnel.",
+  author: "Adeline Pierrot",
+  keywords: "Adeline Pierrot, Cabinet, Cabinet de soins, Kinésiologie, Ayurvéda, Lithothérapie",
+  openGraph: {
+    type: "website",
+    url: "https://www.adhyanga.fr",
+    title: "Adhyanga - Votre cabinet de soin kinésiologique et ayurvédique sur Angoulême",
+    description: "Adhyanga - Cabinet de soin kinésiologique et ayurvédique à Angoulême. Découvrez une approche holistique avec Adeline Pierrot, praticienne expérimentée, pour harmoniser votre bien-être physique et émotionnel.",
+    image: "https://adhyanga.fr/images/logoRemoved.png"
+  },
+  twitter: {
+    url: "https://www.adhyanga.fr",
+    title: "Adhyanga - Votre cabinet de soin kinésiologique et ayurvédique sur Angoulême",
+    description: "Adhyanga - Cabinet de soin kinésiologique et ayurvédique à Angoulême. Découvrez une approche holistique avec Adeline Pierrot, praticienne expérimentée, pour harmoniser votre bien-être physique et émotionnel."
+  },
+  icons: {
+    icon: '/images/logoRemoved.png'
+  }
 };
 
 export default function RootLayout({ children }) {
