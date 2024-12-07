d8c9377ac026504220794ded8a8177e96f510a51
allistair <allistair.jak@gmail.com>
2024-12-07 13:24:07 +0100

    feat: create a sveltekit skeleton project
    - created with vite
    - include prettier and eslint
    - add the jsDoc conventions for typing

---
+++ .svelte-kit/ambient.d.ts
@@ -0,0 +1,375 @@
+
+// this file is generated — do not edit it
+
+
+/// <reference types="@sveltejs/kit" />
+
+/**
+ * Environment variables [loaded by Vite](https://vitejs.dev/guide/env-and-mode.html#env-files) from `.env` files and `process.env`. Like [`$env/dynamic/private`](https://svelte.dev/docs/kit/$env-dynamic-private), this module cannot be imported into client-side code. This module only includes variables that _do not_ begin with [`config.kit.env.publicPrefix`](https://svelte.dev/docs/kit/configuration#env) _and do_ start with [`config.kit.env.privatePrefix`](https://svelte.dev/docs/kit/configuration#env) (if configured).
+ * 
+ * _Unlike_ [`$env/dynamic/private`](https://svelte.dev/docs/kit/$env-dynamic-private), the values exported from this module are statically injected into your bundle at build time, enabling optimisations like dead code elimination.
+ * 
+ * ```ts
+ * import { API_KEY } from '$env/static/private';
+ * ```
+ * 
+ * Note that all environment variables referenced in your code should be declared (for example in an `.env` file), even if they don't have a value until the app is deployed:
+ * 
+ * ```
+ * MY_FEATURE_FLAG=""
+ * ```
+ * 
+ * You can override `.env` values from the command line like so:
+ * 
+ * ```bash
+ * MY_FEATURE_FLAG="enabled" npm run dev
+ * ```
+ */
+declare module '$env/static/private' {
+	export const npm_package_exports___node_polyfills_types: string;
+	export const npm_package_scripts_test_cross_platform_build: string;
+	export const rvm_use_flag: string;
+	export const npm_package_exports___vite_import: string;
+	export const npm_package_exports___hooks_import: string;
+	export const TERM_PROGRAM: string;
+	export const rvm_bin_path: string;
+	export const NODE: string;
+	export const npm_package_dependencies_sade: string;
+	export const INIT_CWD: string;
+	export const npm_package_devDependencies_typescript: string;
+	export const npm_package_homepage: string;
+	export const _P9K_TTY: string;
+	export const rvm_quiet_flag: string;
+	export const npm_package_devDependencies_vite: string;
+	export const SHELL: string;
+	export const TERM: string;
+	export const rvm_gemstone_url: string;
+	export const npm_package_dependencies_devalue: string;
+	export const TMPDIR: string;
+	export const rvm_docs_type: string;
+	export const npm_package_scripts_lint: string;
+	export const npm_package_dependencies_set_cookie_parser: string;
+	export const TERM_PROGRAM_VERSION: string;
+	export const npm_package_dependencies_cookie: string;
+	export const MallocNanoZone: string;
+	export const ORIGINAL_XDG_CURRENT_DESKTOP: string;
+	export const rvm_hook: string;
+	export const npm_config_registry: string;
+	export const npm_package_devDependencies_svelte_preprocess: string;
+	export const npm_package_dependencies_import_meta_resolve: string;
+	export const npm_package_repository_url: string;
+	export const ZSH: string;
+	export const PNPM_HOME: string;
+	export const npm_package_exports___node_import: string;
+	export const npm_package_description: string;
+	export const USER: string;
+	export const npm_package_exports___package_json: string;
+	export const npm_package_dependencies_esm_env: string;
+	export const npm_package_license: string;
+	export const LS_COLORS: string;
+	export const rvm_gemstone_package_file: string;
+	export const COMMAND_MODE: string;
+	export const PNPM_SCRIPT_SRC_DIR: string;
+	export const npm_package_exports___import: string;
+	export const npm_package_repository_directory: string;
+	export const rvm_path: string;
+	export const SSH_AUTH_SOCK: string;
+	export const npm_package_bin_svelte_kit: string;
+	export const __CF_USER_TEXT_ENCODING: string;
+	export const npm_execpath: string;
+	export const rvm_proxy: string;
+	export const npm_package_peerDependencies__sveltejs_vite_plugin_svelte: string;
+	export const npm_package_devDependencies_svelte: string;
+	export const PAGER: string;
+	export const rvm_ruby_file: string;
+	export const npm_config_frozen_lockfile: string;
+	export const LSCOLORS: string;
+	export const rvm_prefix: string;
+	export const rvm_silent_flag: string;
+	export const npm_package_scripts_postinstall: string;
+	export const PATH: string;
+	export const rvm_ruby_make: string;
+	export const npm_package_devDependencies_rollup: string;
+	export const npm_package_dependencies_magic_string: string;
+	export const npm_config_engine_strict: string;
+	export const npm_package_keywords_4: string;
+	export const __CFBundleIdentifier: string;
+	export const npm_command: string;
+	export const PWD: string;
+	export const npm_package_bugs_url: string;
+	export const npm_lifecycle_event: string;
+	export const P9K_SSH: string;
+	export const npm_package_types: string;
+	export const npm_package_devDependencies__sveltejs_vite_plugin_svelte: string;
+	export const npm_package_repository_type: string;
+	export const npm_package_keywords_0: string;
+	export const npm_package_name: string;
+	export const LANG: string;
+	export const P9K_TTY: string;
+	export const rvm_sdk: string;
+	export const npm_package_scripts_generate_types: string;
+	export const npm_package_scripts_test_integration: string;
+	export const npm_package_devDependencies__types_connect: string;
+	export const npm_package_keywords_1: string;
+	export const npm_package_exports___node_polyfills_import: string;
+	export const npm_package_exports___types: string;
+	export const npm_package_keywords_2: string;
+	export const NODE_PATH: string;
+	export const npm_package_scripts_test_cross_platform_dev: string;
+	export const npm_package_devDependencies_vitest: string;
+	export const npm_package_keywords_3: string;
+	export const XPC_FLAGS: string;
+	export const npm_package_dependencies_tiny_glob: string;
+	export const npm_package_engines_node: string;
+	export const npm_package_dependencies_sirv: string;
+	export const npm_config_node_gyp: string;
+	export const npm_package_version: string;
+	export const XPC_SERVICE_NAME: string;
+	export const rvm_version: string;
+	export const npm_package_type: string;
+	export const HOME: string;
+	export const SHLVL: string;
+	export const rvm_pretty_print_flag: string;
+	export const rvm_script_name: string;
+	export const npm_package_scripts_generate_version: string;
+	export const npm_package_scripts_test: string;
+	export const npm_package_scripts_check_all: string;
+	export const rvm_ruby_mode: string;
+	export const npm_package_exports___vite_types: string;
+	export const npm_package_exports___hooks_types: string;
+	export const npm_package_scripts_format: string;
+	export const npm_package_peerDependencies_vite: string;
+	export const LOGNAME: string;
+	export const LESS: string;
+	export const npm_lifecycle_script: string;
+	export const npm_package_peerDependencies_svelte: string;
+	export const rvm_alias_expanded: string;
+	export const npm_package_devDependencies__types_set_cookie_parser: string;
+	export const BUN_INSTALL: string;
+	export const npm_config_user_agent: string;
+	export const npm_package_files_3: string;
+	export const npm_package_dependencies__types_cookie: string;
+	export const rvm_nightly_flag: string;
+	export const npm_package_files_2: string;
+	export const npm_package_devDependencies__types_node: string;
+	export const npm_package_devDependencies__playwright_test: string;
+	export const rvm_ruby_make_install: string;
+	export const npm_package_files_1: string;
+	export const npm_package_devDependencies_dts_buddy: string;
+	export const _P9K_SSH_TTY: string;
+	export const rvm_niceness: string;
+	export const npm_package_files_0: string;
+	export const rvm_ruby_bits: string;
+	export const npm_package_dependencies_mrmime: string;
+	export const npm_package_dependencies_kleur: string;
+	export const rvm_bin_flag: string;
+	export const npm_package_exports___node_types: string;
+	export const npm_package_files_6: string;
+	export const rvm_only_path_flag: string;
+	export const npm_package_scripts_check: string;
+	export const npm_package_files_5: string;
+	export const npm_node_execpath: string;
+	export const npm_package_scripts_test_unit: string;
+	export const npm_package_files_4: string;
+	export const COLORTERM: string;
+	export const _: string;
+}
+
+/**
+ * Similar to [`$env/static/private`](https://svelte.dev/docs/kit/$env-static-private), except that it only includes environment variables that begin with [`config.kit.env.publicPrefix`](https://svelte.dev/docs/kit/configuration#env) (which defaults to `PUBLIC_`), and can therefore safely be exposed to client-side code.
+ * 
+ * Values are replaced statically at build time.
+ * 
+ * ```ts
+ * import { PUBLIC_BASE_URL } from '$env/static/public';
+ * ```
+ */
+declare module '$env/static/public' {
+	
+}
+
+/**
+ * This module provides access to runtime environment variables, as defined by the platform you're running on. For example if you're using [`adapter-node`](https://github.com/sveltejs/kit/tree/main/packages/adapter-node) (or running [`vite preview`](https://svelte.dev/docs/kit/cli)), this is equivalent to `process.env`. This module only includes variables that _do not_ begin with [`config.kit.env.publicPrefix`](https://svelte.dev/docs/kit/configuration#env) _and do_ start with [`config.kit.env.privatePrefix`](https://svelte.dev/docs/kit/configuration#env) (if configured).
+ * 
+ * This module cannot be imported into client-side code.
+ * 
+ * Dynamic environment variables cannot be used during prerendering.
+ * 
+ * ```ts
+ * import { env } from '$env/dynamic/private';
+ * console.log(env.DEPLOYMENT_SPECIFIC_VARIABLE);
+ * ```
+ * 
+ * > In `dev`, `$env/dynamic` always includes environment variables from `.env`. In `prod`, this behavior will depend on your adapter.
+ */
+declare module '$env/dynamic/private' {
+	export const env: {
+		npm_package_exports___node_polyfills_types: string;
+		npm_package_scripts_test_cross_platform_build: string;
+		rvm_use_flag: string;
+		npm_package_exports___vite_import: string;
+		npm_package_exports___hooks_import: string;
+		TERM_PROGRAM: string;
+		rvm_bin_path: string;
+		NODE: string;
+		npm_package_dependencies_sade: string;
+		INIT_CWD: string;
+		npm_package_devDependencies_typescript: string;
+		npm_package_homepage: string;
+		_P9K_TTY: string;
+		rvm_quiet_flag: string;
+		npm_package_devDependencies_vite: string;
+		SHELL: string;
+		TERM: string;
+		rvm_gemstone_url: string;
+		npm_package_dependencies_devalue: string;
+		TMPDIR: string;
+		rvm_docs_type: string;
+		npm_package_scripts_lint: string;
+		npm_package_dependencies_set_cookie_parser: string;
+		TERM_PROGRAM_VERSION: string;
+		npm_package_dependencies_cookie: string;
+		MallocNanoZone: string;
+		ORIGINAL_XDG_CURRENT_DESKTOP: string;
+		rvm_hook: string;
+		npm_config_registry: string;
+		npm_package_devDependencies_svelte_preprocess: string;
+		npm_package_dependencies_import_meta_resolve: string;
+		npm_package_repository_url: string;
+		ZSH: string;
+		PNPM_HOME: string;
+		npm_package_exports___node_import: string;
+		npm_package_description: string;
+		USER: string;
+		npm_package_exports___package_json: string;
+		npm_package_dependencies_esm_env: string;
+		npm_package_license: string;
+		LS_COLORS: string;
+		rvm_gemstone_package_file: string;
+		COMMAND_MODE: string;
+		PNPM_SCRIPT_SRC_DIR: string;
+		npm_package_exports___import: string;
+		npm_package_repository_directory: string;
+		rvm_path: string;
+		SSH_AUTH_SOCK: string;
+		npm_package_bin_svelte_kit: string;
+		__CF_USER_TEXT_ENCODING: string;
+		npm_execpath: string;
+		rvm_proxy: string;
+		npm_package_peerDependencies__sveltejs_vite_plugin_svelte: string;
+		npm_package_devDependencies_svelte: string;
+		PAGER: string;
+		rvm_ruby_file: string;
+		npm_config_frozen_lockfile: string;
+		LSCOLORS: string;
+		rvm_prefix: string;
+		rvm_silent_flag: string;
+		npm_package_scripts_postinstall: string;
+		PATH: string;
+		rvm_ruby_make: string;
+		npm_package_devDependencies_rollup: string;
+		npm_package_dependencies_magic_string: string;
+		npm_config_engine_strict: string;
+		npm_package_keywords_4: string;
+		__CFBundleIdentifier: string;
+		npm_command: string;
+		PWD: string;
+		npm_package_bugs_url: string;
+		npm_lifecycle_event: string;
+		P9K_SSH: string;
+		npm_package_types: string;
+		npm_package_devDependencies__sveltejs_vite_plugin_svelte: string;
+		npm_package_repository_type: string;
+		npm_package_keywords_0: string;
+		npm_package_name: string;
+		LANG: string;
+		P9K_TTY: string;
+		rvm_sdk: string;
+		npm_package_scripts_generate_types: string;
+		npm_package_scripts_test_integration: string;
+		npm_package_devDependencies__types_connect: string;
+		npm_package_keywords_1: string;
+		npm_package_exports___node_polyfills_import: string;
+		npm_package_exports___types: string;
+		npm_package_keywords_2: string;
+		NODE_PATH: string;
+		npm_package_scripts_test_cross_platform_dev: string;
+		npm_package_devDependencies_vitest: string;
+		npm_package_keywords_3: string;
+		XPC_FLAGS: string;
+		npm_package_dependencies_tiny_glob: string;
+		npm_package_engines_node: string;
+		npm_package_dependencies_sirv: string;
+		npm_config_node_gyp: string;
+		npm_package_version: string;
+		XPC_SERVICE_NAME: string;
+		rvm_version: string;
+		npm_package_type: string;
+		HOME: string;
+		SHLVL: string;
+		rvm_pretty_print_flag: string;
+		rvm_script_name: string;
+		npm_package_scripts_generate_version: string;
+		npm_package_scripts_test: string;
+		npm_package_scripts_check_all: string;
+		rvm_ruby_mode: string;
+		npm_package_exports___vite_types: string;
+		npm_package_exports___hooks_types: string;
+		npm_package_scripts_format: string;
+		npm_package_peerDependencies_vite: string;
+		LOGNAME: string;
+		LESS: string;
+		npm_lifecycle_script: string;
+		npm_package_peerDependencies_svelte: string;
+		rvm_alias_expanded: string;
+		npm_package_devDependencies__types_set_cookie_parser: string;
+		BUN_INSTALL: string;
+		npm_config_user_agent: string;
+		npm_package_files_3: string;
+		npm_package_dependencies__types_cookie: string;
+		rvm_nightly_flag: string;
+		npm_package_files_2: string;
+		npm_package_devDependencies__types_node: string;
+		npm_package_devDependencies__playwright_test: string;
+		rvm_ruby_make_install: string;
+		npm_package_files_1: string;
+		npm_package_devDependencies_dts_buddy: string;
+		_P9K_SSH_TTY: string;
+		rvm_niceness: string;
+		npm_package_files_0: string;
+		rvm_ruby_bits: string;
+		npm_package_dependencies_mrmime: string;
+		npm_package_dependencies_kleur: string;
+		rvm_bin_flag: string;
+		npm_package_exports___node_types: string;
+		npm_package_files_6: string;
+		rvm_only_path_flag: string;
+		npm_package_scripts_check: string;
+		npm_package_files_5: string;
+		npm_node_execpath: string;
+		npm_package_scripts_test_unit: string;
+		npm_package_files_4: string;
+		COLORTERM: string;
+		_: string;
+		[key: `PUBLIC_${string}`]: undefined;
+		[key: `${string}`]: string | undefined;
+	}
+}
+
+/**
+ * Similar to [`$env/dynamic/private`](https://svelte.dev/docs/kit/$env-dynamic-private), but only includes variables that begin with [`config.kit.env.publicPrefix`](https://svelte.dev/docs/kit/configuration#env) (which defaults to `PUBLIC_`), and can therefore safely be exposed to client-side code.
+ * 
+ * Note that public dynamic environment variables must all be sent from the server to the client, causing larger network requests — when possible, use `$env/static/public` instead.
+ * 
+ * Dynamic environment variables cannot be used during prerendering.
+ * 
+ * ```ts
+ * import { env } from '$env/dynamic/public';
+ * console.log(env.PUBLIC_DEPLOYMENT_SPECIFIC_VARIABLE);
+ * ```
+ */
+declare module '$env/dynamic/public' {
+	export const env: {
+		[key: `PUBLIC_${string}`]: string | undefined;
+	}
+}
