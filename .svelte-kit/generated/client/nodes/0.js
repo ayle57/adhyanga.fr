4b57aa1e1803643e39ec1a8cfe89356b54237330
allistair <allistair.jak@gmail.com>
2024-12-17 21:58:41 +0100

    config: setting up a sveltekit project
    adding sass and vitejs plugins to run correctly the application

--- .svelte-kit/generated/client/nodes/0.js
+++ .svelte-kit/generated/client/nodes/0.js
@@ -1,1 +1,1 @@
-export { default as component } from "../../../../node_modules/.pnpm/@sveltejs+kit@2.9.0_@sveltejs+vite-plugin-svelte@5.0.1_svelte@5.8.1_vite@6.0.3__svelte@5.8.1_vite@6.0.3/node_modules/@sveltejs/kit/src/runtime/components/svelte-5/layout.svelte";
+export { default as component } from "../../../../src/routes/+layout.svelte";
