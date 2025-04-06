f8a4b92596e49f8b3c5049cd03c05af40a1ef2d1
allistair <allistair.jak@gmail.com>
2025-04-06 19:12:41 +0200

    feat: add landing page layout with global styles and navbar

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,51 @@
+<nav class="navbar">
+	<div class="navbar-brand">
+		<img src="/images/logo.webp" alt="Logo d'adhyanga" class="navbar-brand__logo" />
+	</div>
+	<ul class="navbar-links">
+		<li class="navbar-links__item">
+			<a href="/">Nos tarifs</a>
+		</li>
+		<li class="navbar-links__item">
+			<a href="/">Nos séances</a>
+		</li>
+		<li class="navbar-links__item">
+			<a href="/">À propos</a>
+		</li>
+		<li class="navbar-links__item">
+			<a href="/">Nous contacter</a>
+		</li>
+		<li class="navbar-links__item">
+			<a href="/" class="btn btn-primary btn-icon">
+				Prendre rendez-vous
+				<img src="/images/icons/phone.svg" alt="Icone de téléphone" />
+			</a>
+		</li>
+	</ul>
+</nav>
+
+<style lang="scss">
+	.navbar {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		padding: 20px 5vw;
+
+		.navbar-brand {
+			flex: 0 0 30%;
+			max-width: 30%;
+
+			.navbar-brand__logo {
+				width: 70px;
+				height: auto;
+			}
+		}
+		.navbar-links {
+			display: flex;
+			justify-content: space-around;
+			align-items: center;
+			flex: 0 0 70%;
+			max-width: 70%;
+		}
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	import Navbar from '$lib/components/Navbar.svelte';
+	import '../styles/global.scss';
+	import '../styles/layout/landingLayout.svelte.scss';
+</script>
+
+<main class="landing-layout">
+	<Navbar />
+</main>
