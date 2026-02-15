a8eeeb2fdc8ff3956f2dfcd7054495670a6b3cb2
ayle57 <allistair.jak@gmail.com>
2026-02-15 16:16:33 +0100

    feat: change customercontroller and index.html.twig to add a pagination with knp_paginator

--- src/Controller/Admin/Crud/CustomersController.php
+++ src/Controller/Admin/Crud/CustomersController.php
@@ -2,15 +2,50 @@
 
 namespace App\Controller\Admin\Crud;
 
+use App\Repository\CustomerRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
 
+#[Route("/admin/", name: "admin_customers_")]
 final class CustomersController extends AbstractController
 {
-    #[Route('/customers', name: 'admin_customers')]
-    public function index(): Response
+    public function __construct(
+        private readonly EntityManagerInterface $entityManager,
+        private readonly CustomerRepository $customerRepository,
+        private readonly SerializerInterface $serializer,
+        private readonly PaginatorInterface $paginator,
+    )
     {
-        return $this->render('admin/customers/index.html.twig');
+    }
+
+    #[Route("customers", name: "index")]
+    public function index(Request $request): Response
+    {
+        $search = $request->query->get('q');
+
+        $queryBuilder = $this->customerRepository->findFiltered($search);
+
+        $pagination = $this->paginator->paginate(
+            $queryBuilder,
+            $request->query->getInt('page', 1),
+            10
+        );
+
+        $data = $this->serializer->normalize(
+            $pagination->getItems(),
+            null,
+            ['groups' => 'customer_table']
+        );
+
+        return $this->render("admin/customers/index.html.twig", [
+            "rows" => $data,
+            "pagination" => $pagination,
+            "search" => $search
+        ]);
     }
 }
